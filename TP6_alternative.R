0fedffbf2c42365ded44f35a1a0af0f825902b7c
vtrapple <victor.trappler@inria.fr>
2019-10-17 11:10:22 +0200

    TP6

--- TP6_alternative.R
+++ TP6_alternative.R
@@ -1,7 +1,7 @@
-# TP6: Alternative si le serveur shiny est inaccessible
+# TP6 Exercice 1 Alternatif si le serveur shiny est inaccessible
 # Éxecutez la fonction suivante (ça ne sert à rien de regarder dedans)
-create.CI <- function(NCI, Nsamples, mu=0, sigma=1, niveau=0.05){
-  samples <- rnorm(NCI * Nsamples, mu, sigma)
+create.CI <- function(NCI, Nsamples, mu=0, variance=1, niveau=0.05){
+  samples <- rnorm(NCI * Nsamples, mu, sqrt(variance))
   samples <- matrix(data = samples, nrow=Nsamples, ncol=NCI)
   mean.vec <- colMeans(samples)
   sd.vec <- sqrt(apply(samples, 2, FUN=var))
@@ -10,22 +10,22 @@ create.CI <- function(NCI, Nsamples, mu=0, sigma=1, niveau=0.05){
   plot(c(mu, mu), c(0, NCI), col='red', type='l', xlab='', ylab='', ylim=c(0.5, NCI+0.5), xlim=c(mu - 4*sigma/sqrt(Nsamples), mu+4*sigma/sqrt(Nsamples)) )
   count = 0
   for (i in 1:NCI){
-    lines(CI[i, ], c(i, i)) 
+    lines(CI[i, ], c(i, i), col='blue') 
     if (mu >= CI[i, 1] & mu <= CI[i, 2]){
       count = count + 1
     }
   }
-  percent = 100*(1- count / NCI)
+  percent = round(100*(1- count / NCI))
   str = sprintf("Il y a %s %% d'IC qui ne contiennent par la vraie valeur mu=%s", percent, mu)
   title(str)
   abline(v=mu, col='red')
-  
-  }
+}
 
-
-NombreIntervallesdeConfiance <- 1000
-NombreEchantillonsparIC <- 1000
-muVrai <- 0
-varianceVraie <- 10
-niveaudeconfiance <- 20
-create.CI(NombreIntervallesdeConfiance, NombreEchantillonsparIC, muVrai, varianceVraie, niveaudeconfiance/100)
+# Changez les paramètres ici
+nombre.Echantillons <- 100
+taille.Echantillons <- 50
+mu.theorique <- 0
+variance.theorique <- 1
+niveau.confiance <- 0.95
+# Puis appelez cette fonction
+create.CI(nombre.Echantillons, taille.Echantillons, mu.theorique, variance.theorique, 1-niveau.confiance)
